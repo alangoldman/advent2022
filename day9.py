26534d471172ffb87cd258f1e147ca8de0db6c5f
Alan Goldman <alan.goldman@gatech.edu>
2022-12-25 17:06:01 -0500

    day9 done

--- day9.py
+++ day9.py
@@ -8,39 +8,39 @@ lines = [l.strip() for l in lines]
 
 seen = set()
 
-head = [0, 0]
-tail = [0, 0]
+def move_section(head, tail):
+    dx = abs(head[0] - tail[0])
+    dy = abs(head[1] - tail[1])
+    far = dx > 1 or dy > 1
+    #print('head moves', line, head, tail, far)
+    
+    if far:
+        if dx >= 1 and dy >= 1:
+            # diag case
+            if dx == 1:
+                tail[0] = head[0]
+            if dy == 1:
+                tail[1] = head[1]
+        if dx > 1:
+            tail[0] = int((head[0] + tail[0])/2)
+        if dy > 1:
+            tail[1] = int((head[1] + tail[1])/2)
+
+snake = np.zeros((10, 2))
 for line in lines:
     for i in range(int(line[2:])):
+        # move head
         if line[0] == 'R':
-            head[0] += 1
+            snake[0][0] += 1
         if line[0] == 'U':
-            head[1] += 1
+            snake[0][1] += 1
         if line[0] == 'L':
-            head[0] -= 1
+            snake[0][0] -= 1
         if line[0] == 'D':
-            head[1] -= 1
-            
-        dx = abs(head[0] - tail[0])
-        dy = abs(head[1] - tail[1])
-        far = dx > 1 or dy > 1
-        #print('head moves', line, head, tail, far)
-        
-        if far:
-            if dx >= 1 and dy >= 1:
-                # diag case
-                if dx == 1:
-                    tail[0] = head[0]
-                if dy == 1:
-                    tail[1] = head[1]
-            if dx > 1:
-                tail[0] = int((head[0] + tail[0])/2)
-            if dy > 1:
-                tail[1] = int((head[1] + tail[1])/2)
+            snake[0][1] -= 1   
 
-        dx = abs(head[0] - tail[0])
-        dy = abs(head[1] - tail[1])
-        far = dx > 1 or dy > 1
-        print('tail moves', line, head, tail, far, dx, dy)
-        seen.add((tail[0], tail[1]))
+        # move each tail piece
+        for i in range(1, len(snake)):
+            move_section(snake[i-1], snake[i])
+        seen.add((snake[-1][0], snake[-1][1]))
 print(len(seen))
