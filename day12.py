a8729d59ba51d2a839a43d7de5506433d234be4b
Alan Goldman <alan.goldman@gatech.edu>
2023-01-01 19:14:42 -0500

    day12 part 2

--- day12.py
+++ day12.py
@@ -11,13 +11,12 @@ grid = np.zeros(shape=(len(lines), len(lines[0])))
 for r in range(len(lines)):
     for c in range(len(lines[0])):
         heightmap[r, c] = ord(lines[r][c])
-        if lines[r][c] == 'S':
-            start = (r, c)
         if lines[r][c] == 'E':
-            end = (r, c)
-            
-heightmap[start] = ord('a')
-heightmap[end] = ord('z')
+            start = (r, c)
+            heightmap[start] = ord('z')
+        if lines[r][c] == 'S':
+            heightmap[r, c] = ord('a')
+
 queue = list()
 queue.append(start)
 
@@ -28,10 +27,11 @@ while len(queue) > 0:
     for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
         r = cur[0] + dy
         c = cur[1] + dx
-        if 0 <= r < heightmap.shape[0] and 0 <= c < heightmap.shape[1] and (heightmap[r, c] <= h+1) and grid[r, c] == 0:
+        if 0 <= r < heightmap.shape[0] and 0 <= c < heightmap.shape[1] and (heightmap[r, c] >= h-1) and grid[r, c] == 0:
             grid[r, c] = steps + 1
             queue.append((r, c))
-            if r == end[0] and c == end[1]:
+            if heightmap[r, c] == ord('a'):
                 print(steps+1)
+                queue = [] # exit
                 break
     
