a54bc674f38fd8ade22d45a0e5f610fc77a64c64
Alan Goldman <alan.goldman@gatech.edu>
2023-01-17 22:43:16 -0500

    BFS is too long

--- day16.py
+++ day16.py
@@ -15,13 +15,13 @@ for line in lines:
     
     adj = [a.strip() for a in second.replace('valve', 'valves')[24:].split(', ')]
     
-    graph[valve] = (rate, adj, -1)
+    graph[valve] = (rate, adj)
     
 queue = list()
 seen = set()
 # steps, score, position, opened valves
-queue.append((30, 0, 'AA', set()))
-seen.add((0, 'AA', ''))
+queue.append((26, 0, 'AA', 'AA', True, set()))
+seen.add((0, 'AA', 'AA', True, ''))
 best = None
 print(graph)
 
@@ -29,11 +29,11 @@ def set_to_string(s):
     return ''.join([c for c in s])
 
 while len(queue)>0:
-    steps, score, cur, valves = queue.pop(0)
-    steps -= 1
+    steps, score, cur, ele, my_turn, valves = queue.pop(0)
+    if my_turn:
+        steps -= 1
 
-    flow, adj, _ = graph[cur]
-    #print(steps, score, cur, valves)
+    #print(steps, score, cur, ele, my_turn, valves)
     if steps == 0 or len(valves) == len(graph.keys()):
         if  len(valves) == len(graph.keys()):
             print('early!')
@@ -41,17 +41,36 @@ while len(queue)>0:
             best = score
         continue
     
-    if cur not in valves:
-        # try opening the valve
-        new_score = score+flow*steps
-        valves2 = valves.union(set([cur]))
-        if (new_score, cur, set_to_string(valves)) not in seen:
-            seen.add((new_score, cur, set_to_string(valves2)))
-            queue.append((steps, new_score, cur, valves2))
+    if my_turn:
+        flow, adj = graph[cur]
+        next_states = adj if cur in valves else adj + ['open']
+        
+        for edge in next_states:
+            if edge == 'open':
+                # try opening the valve
+                new_score = score+flow*steps
+                valves2 = valves.union(set([cur]))
+                if (new_score, cur, ele, my_turn, set_to_string(valves2)) not in seen:
+                    seen.add((new_score, cur, ele, my_turn, set_to_string(valves2)))
+                    queue.append((steps, new_score, cur, ele, False, valves2))
+            elif (score, edge, ele, my_turn, set_to_string(valves)) not in seen:
+                seen.add((score, edge, ele, my_turn, set_to_string(valves)))
+                queue.append((steps, score, edge, ele, False, valves))
+    else:
+        flow, adj = graph[ele]
+        next_states = adj if ele in valves else adj + ['open']
+        
+        for edge in next_states:
+            if edge == 'open':
+                # try opening the valve
+                new_score = score+flow*steps
+                valves2 = valves.union(set([ele]))
+                if (new_score, cur, ele, my_turn, set_to_string(valves2)) not in seen:
+                    seen.add((new_score, cur, ele, my_turn, set_to_string(valves2)))
+                    queue.append((steps, new_score, cur, ele, True, valves2))
+            elif (score, cur, edge, my_turn, set_to_string(valves)) not in seen:
+                seen.add((score, cur, edge, my_turn, set_to_string(valves)))
+                queue.append((steps, score, cur, edge, True, valves))
 
-    for edge in adj:
-        if (score, edge, set_to_string(valves)) not in seen:
-            seen.add((score, edge, set_to_string(valves)))
-            queue.append((steps, score, edge, valves))
 
 print(best)
