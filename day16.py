537eba50f5c425577c300f31e636cf6d4b13bf08
Alan Goldman <alan.goldman@gatech.edu>
2023-01-28 14:41:02 -0500

    1993 too low

--- day16.py
+++ day16.py
@@ -39,22 +39,32 @@ for k in range(0, len(valve_list)):
 queue = list()
 seen = set()
 # steps, score, position, opened valves
-queue.append((30, 0, 'AA', set()))
-seen.add((30, 'AA', ''))
-best = None
+queue.append((26, 26, 0, 'AA', 'AA', set()))
+#seen.add((0, 'AA', 'AA', ''))
+best = 0
 print(graph)
 
 def set_to_string(s):
     return ''.join([c for c in s])
+    
+def best_possible_remaining_score(steps, valves):
+    score = 0
+    for valve in graph.keys():
+        if valve not in valves:
+            score += (steps-1) * graph[valve][0]
+    return score
 
 while len(queue)>0:
-    steps, score, cur, valves = queue.pop(0)
+    steps, steps_e, score, cur, cur_e, valves = queue.pop()
 
+    #print(steps, steps_e, score, cur, cur_e, valves)
+        
+    if score + best_possible_remaining_score(max(steps, steps_e), valves) < best:
+        continue
+
+    next_moves = []
+    # my turn
     flow, adj, index = graph[cur]
-    #print(steps, score, cur, valves)
-    if best is None or score > best:
-        best = score
-
     for valve in graph.keys():
         if graph[valve][0] > 0 and valve not in valves:
             steps2 = steps - dist[graph[cur][2], graph[valve][2]] - 1
@@ -62,8 +72,25 @@ while len(queue)>0:
                 continue
             score2 = score + steps2 * graph[valve][0]
             valves2 = valves.union(set([valve]))
-            if (steps2, valve, set_to_string(valves2)) not in seen:
-                seen.add((score2, steps2, set_to_string(valves2)))
-                queue.append((steps2, score2, valve, valves2))
+            #if (score2, valve, cur_e, set_to_string(valves2)) not in seen:
+                #seen.add((score2, valve, cur_e, set_to_string(valves2)))
+            next_moves.append((steps2, steps_e, score2, valve, cur_e, valves2))
+                
+    flow, adj, index = graph[cur_e]
+    for valve in graph.keys():
+        if graph[valve][0] > 0 and valve not in valves:
+            steps2 = steps_e - dist[graph[cur_e][2], graph[valve][2]] - 1
+            if steps2 < 0:
+                continue
+            score2 = score + steps2 * graph[valve][0]
+            valves2 = valves.union(set([valve]))
+            #if (score2, cur, valve, set_to_string(valves2)) not in seen:
+                #seen.add((score2, cur, valve, set_to_string(valves2)))
+            next_moves.append((steps, steps2, score2, cur, valve, valves2))
+               
+    if len(next_moves) == 0:
+        best = max(best, score)
+    next_moves = sorted(next_moves, key=lambda x: x[2], reverse=True)
+    queue += next_moves
 
 print(best)
