2a3f372202fd50449a73fd26776bf19bf58aa833
Alan Goldman <alan.goldman@gatech.edu>
2023-01-28 10:57:04 -0500

    Working day16 part 1 with FW

--- day16.py
+++ day16.py
@@ -7,6 +7,8 @@ file.close()
 
 lines = [l.strip() for l in lines]
 
+valve_index = 0
+valve_list = []
 graph = {}
 for line in lines:
     first, second = line.split(';')
@@ -15,13 +17,30 @@ for line in lines:
     
     adj = [a.strip() for a in second.replace('valve', 'valves')[24:].split(', ')]
     
-    graph[valve] = (rate, adj, -1)
+    graph[valve] = (rate, adj, valve_index)
+    valve_list.append(valve)
+    valve_index += 1
     
+dist = np.full((len(graph.keys()), len(graph.keys())), 1000000)
+
+# floyd-warshall
+for valve in graph.keys():
+    flow, adj, index = graph[valve]
+    dist[index][index] = 0
+    for edge in adj:
+        dist[index][graph[edge][2]] = 1
+for k in range(0, len(valve_list)):
+    for i in range(0, len(valve_list)):
+        for j in range(0, len(valve_list)):
+            composite_dist = dist[i, k] + dist[k, j]
+            dist[i, j] = min(dist[i, j], composite_dist)
+
+
 queue = list()
 seen = set()
 # steps, score, position, opened valves
 queue.append((30, 0, 'AA', set()))
-seen.add((0, 'AA', ''))
+seen.add((30, 'AA', ''))
 best = None
 print(graph)
 
@@ -30,28 +49,21 @@ def set_to_string(s):
 
 while len(queue)>0:
     steps, score, cur, valves = queue.pop(0)
-    steps -= 1
 
-    flow, adj, _ = graph[cur]
+    flow, adj, index = graph[cur]
     #print(steps, score, cur, valves)
-    if steps == 0 or len(valves) == len(graph.keys()):
-        if  len(valves) == len(graph.keys()):
-            print('early!')
-        if best is None or score > best:
-            best = score
-        continue
-    
-    if cur not in valves:
-        # try opening the valve
-        new_score = score+flow*steps
-        valves2 = valves.union(set([cur]))
-        if (new_score, cur, set_to_string(valves)) not in seen:
-            seen.add((new_score, cur, set_to_string(valves2)))
-            queue.append((steps, new_score, cur, valves2))
+    if best is None or score > best:
+        best = score
 
-    for edge in adj:
-        if (score, edge, set_to_string(valves)) not in seen:
-            seen.add((score, edge, set_to_string(valves)))
-            queue.append((steps, score, edge, valves))
+    for valve in graph.keys():
+        if graph[valve][0] > 0 and valve not in valves:
+            steps2 = steps - dist[graph[cur][2], graph[valve][2]] - 1
+            if steps2 < 0:
+                continue
+            score2 = score + steps2 * graph[valve][0]
+            valves2 = valves.union(set([valve]))
+            if (steps2, valve, set_to_string(valves2)) not in seen:
+                seen.add((score2, steps2, set_to_string(valves2)))
+                queue.append((steps2, score2, valve, valves2))
 
 print(best)
